c518721cb3d89ed969924099859d5b69ad21b1d9
SeongHyun Koo <koosh@SeongHyunui-MacBookPro.local>
2023-08-16 22:44:24 +0900

    feat: Initiate java project

---
+++ src/main/java/com/deadcow5/alarm/AlarmApplication.java
@@ -0,0 +1,57 @@
+package com.deadcow5.alarm;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.deadcow5.alarm.service.Receiver;
+
+@SpringBootApplication
+public class AlarmApplication {
+	
+	static final String topicExchangeName = "alarm-exchange";
+	static final String queueName =  "alarm-queue";
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+	
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange(topicExchangeName);
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("alarm.key.#");
+	}
+
+	@Bean
+	SimpleMessageListenerContainer container(
+		ConnectionFactory connectionFactory, 
+		MessageListenerAdapter listenerAdapter
+		) {
+    SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+    container.setConnectionFactory(connectionFactory);
+    container.setQueueNames(queueName);
+    container.setMessageListener(listenerAdapter);
+    return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(AlarmApplication.class, args);
+	}
+}
