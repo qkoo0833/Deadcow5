2d6859db6a0ad7834bab23f9440b538f5d684ee3
qkoo0833 <qkoo0833@gmail.com>
2023-08-21 23:48:29 +0900

    fix: Not use `user` table name

--- src/main/java/com/deadcow5/alarm/domain/User.java
+++ src/main/java/com/deadcow5/alarm/domain/User.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@Table(name= "user_table")
 public class User {
 
   @Id
